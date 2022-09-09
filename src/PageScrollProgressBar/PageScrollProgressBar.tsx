2e2fc883b7732ee21fd0aa2bacfe01137bff1bd1
Anoop Jadhav <anoop.jadhav@bp.com>
2022-09-09 10:55:12 +0530

    [Anoop] CSS variables added to support color change

--- src/PageScrollProgressBar/PageScrollProgressBar.tsx
+++ src/PageScrollProgressBar/PageScrollProgressBar.tsx
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import styles from "./PageScrollProgressBar.module.css";
 interface Props {
   AppRef: React.RefObject<HTMLDivElement>;
+  progressBarColor: string;
+  progressBarBgColor: string;
+  progressBarHeight: string;
 }
-export const PageScrollProgressBar = ({ AppRef }: Props) => {
+export const PageScrollProgressBar = ({ AppRef, progressBarColor, progressBarBgColor, progressBarHeight }: Props) => {
   const [scrollPosition, setscrollPosition] = useState<number>(0);
+
+  const cssStyles = useMemo(() => ({
+    "--page-scroll-bar-color": progressBarColor || "#eb5757",
+    "--page-scroll-background-color": progressBarBgColor || "#f2f2f2",
+    "--page-scroll-height": progressBarHeight || "0.25rem"
+  }),[progressBarBgColor, progressBarColor, progressBarHeight])
 
   const handleScroll = () => {
     const scrollYHeight = window.scrollY;
@@ -21,6 +30,6 @@ export const PageScrollProgressBar = ({ AppRef }: Props) => {
     };
   }, []);
 
-  return <progress className={styles.pageScroll} id="top-scroll-bar" value={scrollPosition} max={100} />;
+  return <progress className={styles.pageScroll} style={cssStyles as React.CSSProperties}  id="top-scroll-bar" data-testid="top-scroll-bar" value={scrollPosition} max={100} />;
 };
 export default PageScrollProgressBar;
